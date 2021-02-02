18e30fc88a399f2e71d92ce6227e85e04aebaa6c
seeni7 <Seeni.rengasamy@gmail.com>
2021-02-02 10:29:37 -0500

    error handling

--- using_try.py
+++ using_try.py
@@ -1,3 +1,18 @@
 import sys
+import random
 
-print(f"Received Argument {sys.argv[1]}")
+try:
+    print(f"First Argument {sys.argv[1]}")
+    args = sys.argv
+    random.shuffle(args)
+    print(f"Random arguments {args[0]}")
+except (IndexError, KeyError) as err:
+    print(f"Error: no arguments, please provide at least once ({err})")
+#    sys.exit(1)
+except NameError:
+    print(f"Error: random module not loaded")
+#   sys.exit(1)
+else:
+    print("else is running")
+finally:
+    print("finally running")
